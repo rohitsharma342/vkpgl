6bfc82b1b6b911d628fd0b2febd1f93a1b785281
rohitsharma342 <rohitkumar.brainbox@gmail.com>
2025-11-28 05:44:54 +0000

    Updated project for vkpgl

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primaryColor = Color(0xFFDC2626);
+  static const Color primaryColor = Color(0xFF2563EB);
   static const Color backgroundColor = Color(0xFFF8F9FA);
   static const Color cardColor = Colors.white;
   static const Color textPrimary = Color(0xFF2D3436);
   static const Color textSecondary = Color(0xFF636E72);
-  static const Color accentColor = Color(0xFFDC2626);
+  static const Color accentColor = Color(0xFF2563EB);
   static const Color errorColor = Color(0xFFEF4444);
   static const Color successColor = Color(0xFF00B894);
 
   static ThemeData lightTheme = ThemeData(
-    primarySwatch: MaterialColor(0xFFDC2626, {
-      50: const Color(0xFFFEF2F2),
-      100: const Color(0xFFFEE2E2),
-      200: const Color(0xFFFECACA),
-      300: const Color(0xFFFCA5A5),
-      400: const Color(0xFFF87171),
-      500: const Color(0xFFEF4444),
+    primarySwatch: MaterialColor(0xFF2563EB, {
+      50: const Color(0xFFEFF6FF),
+      100: const Color(0xFFDBEAFE),
+      200: const Color(0xFFBFDBFE),
+      300: const Color(0xFF93C5FD),
+      400: const Color(0xFF60A5FA),
+      500: const Color(0xFF3B82F6),
       600: primaryColor,
-      700: const Color(0xFFB91C1C),
-      800: const Color(0xFF991B1B),
-      900: const Color(0xFF7F1D1D),
+      700: const Color(0xFF1D4ED8),
+      800: const Color(0xFF1E40AF),
+      900: const Color(0xFF1E3A8A),
     }),
     primaryColor: primaryColor,
     scaffoldBackgroundColor: backgroundColor,
